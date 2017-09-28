09a98b6376d901a92e9f1dd7140ade1248f8aa7f
ivyridge <ivyridge@LAPTOP-KVU27S0F>
2017-09-28 19:06:56 -0400

    shuffle method update

--- src/edu/buffalo/cse116/Solitaire.java
+++ src/edu/buffalo/cse116/Solitaire.java
@@ -1,18 +1,20 @@
 package edu.buffalo.cse116;
 
-public class Solitare {
+import java.util.Random;
+
+public class Solitaire {
 	
 	private String face; 
 	private int suit;
 	private int value;
 	
-	private Solitare[][] cards = new Solitare[4][13];
-	private Solitare[] deck = new Solitare[52];
+	private Solitaire[][] cards = new Solitaire[4][13];
+	private Solitaire[] deck = new Solitaire[52];
 	
 	private int deckSize;
 	
 	
-	public Solitare(int suit, int value){
+	public Solitaire(int suit, int value){
 		
 		this.suit = suit;
 		this.value = value;
@@ -30,15 +32,26 @@ public void Deck(){
 		
 		for(int suit=0; suit<4; suit++){
 			for(int value=0; value<13; value++){
-				cards[suit][value]= new Solitare(suit+1, value+1);
+				cards[suit][value]= new Solitaire(suit+1, value+1);
 				deck[suit*13+value] = cards[suit][value];
 			}
 		}
 		deckSize=52;
 		
-		// shuffle method will go here
-	
-	
-	
+		
 }
+		
+	private void shuffleDeck(){
+		Random x = new Random();
+		int swap;
+		Solitaire tempSpot;
+	for(int i=0; i<deckSize; i++){
+		swap = x.nextInt(deckSize);
+		tempSpot = deck[swap];
+		deck[swap] = deck[i];
+		deck[i] = tempSpot;
+	}
+}
+	
+
 }
