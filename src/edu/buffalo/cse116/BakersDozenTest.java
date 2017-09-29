4f093cf4af54dd9128fe1d7ab39c9db63b73ad6f
Jonathan Hercules <jherc127@gmail.com>
2017-09-28 20:19:54 -0400

    Updated

--- src/edu/buffalo/cse116/BakersDozenTest.java
+++ src/edu/buffalo/cse116/BakersDozenTest.java
@@ -13,14 +13,11 @@ public class BakersDozenTest {
 		assertEquals("Should return 4 cards initially for each tableau pile",sizeCheck ,4);
 		
 	}
-	public void testFreecellFromTableau() {
-		assert();
-	}
 	
 	@Test // If adding card is legal or illegal and increases number of cards in tableau pile
 	public void testIllegalAdditionToTableau() {
 		boolean illegal = test.getLegal();
-		int sizeCheck = test.getSize();
+		int sizeCheck = test.getTableauSize();
  		assertFalse("Should be false but was true, check that move is illegal,",illegal);
 		assertEquals("Size of tableau should be the same",sizeCheck,4);
 	}
@@ -28,7 +25,7 @@ public class BakersDozenTest {
 	@Test // If adding card is legal or illegal and increases number of cards in tableau pile
 	public void testLegalAdditionToTableau() {
 		boolean legal = test.getLegal();
-		int sizeCheck = test.getSize();
+		int sizeCheck = test.getTableauSize();
  		assertTrue("Should be true but was false,",legal);	
  		assertEquals("Size of tableau should be increase",sizeCheck,3);
 	}
@@ -36,7 +33,7 @@ public class BakersDozenTest {
 	@Test // If adding card is legal or illegal and increases number of cards in tableau pile
 	public void testLegalAdditionToEmptyTableau() {
 		boolean legal = test.getLegal();
-		int sizeCheck = test.getSize();
+		int sizeCheck = test.getTableauSize();
  		assertTrue("Should be true but was false,",legal);
  		assertEquals("Size of tableau should be the increase",sizeCheck,4);
  		
@@ -46,7 +43,7 @@ public class BakersDozenTest {
 	public void testRemoveFromTableau() {
 		boolean removeTest = test.removalIllegal();
 		boolean removeTest2 = test.remove();
-		int sizeCheck = test.getSize();
+		int sizeCheck = test.getTableauSize();
 		assertFalse("Should be an illegal removal but returned" + removeTest,removeTest);
 		assertTrue("Should be an legal removal but returned" + removeTest2,removeTest2);
 		assertEquals("Number of cards should be decreased by 1",sizeCheck,3);
