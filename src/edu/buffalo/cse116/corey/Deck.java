d9f9c32d94124a0aa1f461bafc9e562166a77c18
coreyalm <coreyalm@buffalo.edu>
2017-09-29 18:45:02 -0400

    Added functionality to the deck class

--- src/edu/buffalo/cse116/corey/Deck.java
+++ src/edu/buffalo/cse116/corey/Deck.java
@@ -1,35 +1,59 @@
 package edu.buffalo.cse116;
 
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Collections;
+import java.util.ArrayList;
 
 /**
 * <h1>Deck class</h1> 
 * Defines Deck class which has 52 Card instances.
 * <p>
 * @author Corey Almonte 
-* @version 1.0
-* @since   2017-09-27
+* @version 1.5
+* @since   2017-09-29
 */
-public class Deck extends Card {
-  private Suite suite;
-  private Rank rank;
-  private Card card;  
+public abstract class Deck {
+    private Card card;
+    private Suite suite;
+    private Rank rank;
 
-  private HashMap<Suite, Rank> deck;
-
-  public Deck() {
-    this.deck = new HashMap<Suite, Rank>();
-    for(int i = 1; i <= 4; i++) {
-      for(int j = 1; j <= 13; j++) {
-        suite = suite.values()[i];
-        rank = rank.values()[j];
-        card = new Card(suite, rank);    //{@code} This part is interesting. s.values() loops through s. s.values()[i]. It
-        this.deck.add(card); 
-      }
+    private ArrayList<Card> deck;
+    
+    //The Deck() constructor takes an arrayList an already defined and makes a deck.
+    public Deck() {
+        this.deck = new ArrayList<Card>();
+        for(Suite suite : suite.values()) {
+            for(Rank rank : rank.values()) {
+                card = new Card(suite, rank); //{@code} This part is interesting. s.values() loops through s. s.values()[i]. It
+                this.deck.add(card); 
+            }
+        }
     }
-  }
+
+    void shuffleDeck(ArrayList<Card> deck) { 
+        Collections.shuffle(deck);       
+    }
+
+    abstract void initialSetup();
+    abstract void removeCard();
+    abstract void addACard();
+
+}
+
+
 
 /**
+
+
+public static void main(String[] args) {
+
+
+}
+
+
+
+
+
 
 
 
@@ -61,7 +85,7 @@ public class Deck extends Card {
 
 
  **/ 
-}
+
 
 
 
