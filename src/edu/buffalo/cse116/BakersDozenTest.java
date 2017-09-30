6d56bf2d8ac01ad1b2bc43f8db337c63ebc0933f
ivyridge <ivyridge@LAPTOP-KVU27S0F>
2017-09-29 21:51:07 -0400

    Baker's Dozen with Bryan's code

--- src/edu/buffalo/cse116/BakersDozenTest.java
+++ src/edu/buffalo/cse116/BakersDozenTest.java
@@ -1,8 +1,9 @@
 package edu.buffalo.cse116;
 
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.Test;
+
 
 public class BakersDozenTest {
 	BakersDozen test = new BakersDozen();
@@ -49,6 +50,57 @@ public class BakersDozenTest {
 		assertEquals("Number of cards should be decreased by 1",sizeCheck,3);
 	}
 	
-	
+	/*(Bryan's code)
+	 * This Test case checks the intial homecell pile in baker's dozen game.  The initial size of
+	 * the homecell pile should equal zero.
+	 */
+	@Test 
+	public void bakersdozenIntial(){
+		ArrayList<HomecellPiles> homecell = BakersDozen.getHomecell();
+		assertEquals(0, getHomecell.size());
+}
+	/*
+	 * This Test case checks if adding to an empty homecell pile in baker's dozen is legal.  If the homecell pile is 
+	 * empty the only cards that can be added is an ace.  If it is a card higher than rank 1 then it cannot be added
+	 * to the pile.
+	 */
+	@Test 
+	public void homecelladdBD(){
+		HomecellPiles homecell = BakersDozen.getHomecell().get(0);
+		Card first = new Card("Diamond", 1);
+		Card second = new Card("Spade", 8);
+		homecell.add(first);
+		homecell.add(second);
+		assertTrue("legal", homecell.canAdd(first, bakersdozen));
+		assertFalse("illegal", homecell.canAdd(second, bakersdozen));
+	}
 
+	/*
+	 * This test case checks if removing a card from the homecell pile in baker's dozen is legal.
+	 * It is illegal to remove a card from the pile thus it should be false if removing is tried.
+	 */
+	@Test
+	public void homecellremoveBD{
+		HomecellPiles homecell = BakersDozen.getHomecell().get(1);
+		assertFalse("illegal", homecell.remove(homecell));
+}
+	/*
+	 * This test case checks the function of adding cards on top of each other in baker's dozen game.
+	 * In this test I created 2 cards of chronological order to see if the top card is the last card added.
+	 * 
+	 */
+	@Test
+	public void homecelltopBD{
+		HomecellPiles homecell = BakersDozen.getHomecell().get(2);
+		Card first = new Card("Diamond", 1);
+		Card second = new Card("Diamond", 2);
+		homecell.add(first);
+		assertEquals("new top card", homecell.top(), first);
+		assertEquals("size of pile", homecell.size(), 1);
+		homecell.add(second);
+		assertEquals("new top card", homecell.top(), second);
+		assertEquals("size of pile", homecell.size(), 2);
+	}	
+	
+	
 }
