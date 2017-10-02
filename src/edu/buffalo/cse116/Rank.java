b7b7cb5c6561697ee84133a81b11a99c083a2748
coreyalm <coreyalm@buffalo.edu>
2017-10-02 17:12:28 -0400

    javadocs for rank being done

--- src/edu/buffalo/cse116/Rank.java
+++ src/edu/buffalo/cse116/Rank.java
@@ -1,19 +1,43 @@
 package edu.buffalo.cse116;
 
+/**
+* <h1>Rank</h1>
+* Is an enum representing the values of
+* a card. 
+* <p>
+* <b>Note:</b> This is to be combined with the Suit enum to form
+* the card class.
+*
+* @author  Corey Almonte
+* @version 1.5
+* @since   2017-09-29
+*/
 public enum Rank {
     ACE(1), TWO(2), THREE(3), FOUR(4), 
     FIVE(5), SIX(6), SEVEN(7), EIGHT(8), 
     NINE(9), TEN(10), JACK(11), QUEEN(12),
     KING(13);  
   
+    /**
+    * The value of each enum value.
+    */
     private int rank;
- 
+     
+    /**
+    *
+    *@param rank
+    */
     Rank(int rank) {
       this.rank = rank;
     }
  
     public int getRank() {
       return rank;
+    }
+
+    @Override
+    public String toString() {
+        return getSuit() + " of " + getRank();
     }
 }        
 
