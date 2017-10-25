b39b37e8012c68ea908255a184c7e1c6ec68600c
captainnicholas <man1@buffalo.edu>
2017-10-25 12:05:44 -0400

    the beginning of the board layouts...

--- src/edu/buffalo/cse116/MainMenu.java
+++ src/edu/buffalo/cse116/MainMenu.java
@@ -4,8 +4,10 @@ import java.util.List;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.application.Application;
 import javafx.application.Platform;
 
@@ -13,17 +15,21 @@ import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.Skinnable;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
@@ -31,6 +37,7 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
 import javafx.scene.media.Media;
@@ -62,7 +69,7 @@ public class MainMenu extends Application {
 	private double mainHeight = 500;
 	
 	private Stage stage;
-//	private static MediaPlayer mediaPlayer;
+	private static MediaPlayer mediaPlayer;
 
 	//All gui code can be done within the start method
 	@Override
@@ -74,30 +81,31 @@ public class MainMenu extends Application {
 		text.setFont(Font.font ("Verdana", 150));
 		text.setFill(Color.ALICEBLUE);//
 		
-//		Image image = new Image("http://i0.kym-cdn.com/photos/images/original/001/169/608/a43.gif", true);  								
-//		ImageView imageView = new ImageView(image);			
-//		imageView.setPreserveRatio(true);  
-//		imageView.fitWidthProperty().bind(mainWindow.widthProperty()); 
+	Image image = new Image("http://i0.kym-cdn.com/photos/images/original/001/169/608/a43.gif", true);  								
+		ImageView imageView = new ImageView(image);			
+		imageView.setPreserveRatio(true);  
+		imageView.fitWidthProperty().bind(mainWindow.widthProperty()); 
 
-//		Media sound = new Media("https://www.mfiles.co.uk/mp3-downloads/gabriels-message-keyboard.mp3");
-//		mediaPlayer = new MediaPlayer(sound);
-//		mediaPlayer.setOnEndOfMedia(new Runnable() {
-//			@Override public void run() {
-//				mediaPlayer.seek(Duration.ZERO);
-//			}
-//		});
-//		mediaPlayer.play();
+		Media sound = new Media("https://www.mfiles.co.uk/mp3-downloads/gabriels-message-keyboard.mp3");
+		mediaPlayer = new MediaPlayer(sound);
+		mediaPlayer.setOnEndOfMedia(new Runnable() {
+			@Override public void run() {
+				mediaPlayer.seek(Duration.ZERO);
+			}
+		});
+		mediaPlayer.play();
 
 		BorderPane borderPane = new BorderPane();
-		SolitaireView solitaireView = new SolitaireView();
-		solitaireView.setCardImages();
+		borderPane.getChildren().add(imageView);
+//		SolitaireView solitaireView = new SolitaireView();
+//		solitaireView.setCardImages();
 		
-		borderPane.getChildren().add(solitaireView.getCardImages().get(new Card(Suit.CLUBS,Rank.ACE)));
+//		borderPane.getChildren().add(solitaireView.getCardImages().get(new Card(Suit.CLUBS,Rank.ACE)));
 
 		MenuItem bdMenuItm = new MenuItem("Baker's Dozen");
 		bdMenuItm.setOnAction(new EventHandler<ActionEvent>() {
 		    @Override public void handle(ActionEvent e) {
-//		    	mediaPlayer.stop();
+		    	mediaPlayer.stop();
 		    	bakersDozenGame();
 		    }
 		});
@@ -105,8 +113,10 @@ public class MainMenu extends Application {
 		MenuItem fcMenuItm = new MenuItem("Freecell");
 		fcMenuItm.setOnAction(new EventHandler<ActionEvent>() {
 		    @Override public void handle(ActionEvent e) {
+		    	mediaPlayer.stop();
 		    	Freecell fc = new Freecell(8, 4, 4);
 		    	fc.initialSetup();
+		    	freeCellGame();
 		    }
 		});
 		
@@ -132,7 +142,6 @@ public class MainMenu extends Application {
 		//Each scene object can only have one root node.
 		Scene scene = new Scene(borderPane, mainWidth, mainHeight);  //, mainWidth, mainHeight);
 
-
 		//A window		
 		mainWindow.setTitle("Solitaire.exe");
 
@@ -142,9 +151,6 @@ public class MainMenu extends Application {
 		//Actually displays the contents
 		mainWindow.show();	
 
-
-
-
 		//Stage				 The window (BOOK)
 		//Scene				 The space within the window (like a pane). (A PAGE) 
 		//					 This is supposed to be like an area within the application.
@@ -153,21 +159,137 @@ public class MainMenu extends Application {
 	}
 
 	private void bakersDozenGame() {
+	
     	StackPane stackPane = new StackPane();
     	changeScene(stackPane); 
-    	    	   	
-//		Media sound = new Media("https://www.mfiles.co.uk/mp3-downloads/02.The%20calm%20sea%20floating%20mirage.mp3");
-//		mediaPlayer = new MediaPlayer(sound);
-//		mediaPlayer.setOnEndOfMedia(new Runnable() {
-//			@Override public void run() {
-//		         mediaPlayer.seek(Duration.ZERO);
-//		       }
-//		});
-//		mediaPlayer.play(); 
+    	
+    	BorderPane root = new BorderPane();
+    	Rectangle r = new Rectangle(900, 500, Color.AQUA);
+    	Label title = new Label("Baker's Dozen- to the DEATH");
+    	stackPane.setAlignment(title, Pos.TOP_CENTER);
+    	//Setting the font of the text 
+        title.setFont(Font.font(null, FontWeight.BOLD, 15));     
+        
+        //Setting the color of the text 
+        title.setTextFill(Color.CRIMSON); 
+        
+        GridPane gridpane = new GridPane();
+        
+    	ObservableList list = stackPane.getChildren(); 
+		list.addAll(r, title, gridpane);
+		
+		gridpane.setPadding(new Insets(5));
+	    gridpane.setHgap(50);
+	    gridpane.setVgap(120);
+	    ColumnConstraints column1 = new ColumnConstraints(100);
+	    ColumnConstraints column2 = new ColumnConstraints(50, 150, 300);
+	    column2.setHgrow(Priority.ALWAYS);
+	    gridpane.getColumnConstraints().addAll(column1, column2);
+
+	    Label Lbl1 = new Label("Homecell Piles ->");
+	    Label Lbl2 = new Label("<---- Tableau Piles ---->");
+	    
+	    Button newGame = new Button("New Game");
+	    
+	    GridPane.setHalignment(Lbl1, HPos.RIGHT);
+	    GridPane.setValignment(Lbl1, VPos.CENTER);
+	    gridpane.add(Lbl1, 0, 0);
+	    
+	    GridPane.setHalignment(Lbl2, HPos.CENTER);
+	    gridpane.add(Lbl2, 1, 1);
+	 
+		Scene scene = new Scene(stackPane);
+		// button wont display. Irritating
+		GridPane.setValignment(newGame, VPos.BOTTOM);
+		gridpane.add(newGame, 0, 0);
+		root.setCenter(gridpane);
+		
+		//throwing error here because there's a stage conflict
+		stage.setScene(scene);
+		stage.show();
+	
+		Media sound = new Media("https://www.mfiles.co.uk/mp3-downloads/02.The%20calm%20sea%20floating%20mirage.mp3");
+		mediaPlayer = new MediaPlayer(sound);
+		mediaPlayer.setOnEndOfMedia(new Runnable() {
+			@Override public void run() {
+		         mediaPlayer.seek(Duration.ZERO);
+		       }
+		});
+		mediaPlayer.play(); 
 		
 		BakersDozen bd = new BakersDozen();
     	bd.initialSetup();
 	}
+	
+	public void freeCellGame(){
+		
+		StackPane stackPane = new StackPane();
+    	changeScene(stackPane); 
+    	
+    	BorderPane root = new BorderPane();
+    	//not sure how to bind this to prohibit resizing
+    	Rectangle r = new Rectangle(900, 500, Color.LIGHTGREEN);
+    	
+    	Label title = new Label("Freecell- to the DEATH");
+    	stackPane.setAlignment(title, Pos.BOTTOM_CENTER);
+    	//Setting the font of the text 
+        title.setFont(Font.font(null, FontWeight.BOLD, 15));     
+        
+        //Setting the color of the text 
+        title.setTextFill(Color.CRIMSON); 
+        
+        GridPane gridpane = new GridPane();
+        
+    	ObservableList list = stackPane.getChildren(); 
+		list.addAll(r, title, gridpane);
+		
+		gridpane.setPadding(new Insets(5));
+	    gridpane.setHgap(50);
+	    gridpane.setVgap(120);
+	    ColumnConstraints column1 = new ColumnConstraints(100);
+	    ColumnConstraints column2 = new ColumnConstraints(50, 150, 300);
+	    column2.setHgrow(Priority.ALWAYS);
+	    gridpane.getColumnConstraints().addAll(column1, column2);
+
+	    Button newGame = new Button("New Game");
+	    
+	    Label Lbl1 = new Label("Homecell Piles ->");
+	    Label Lbl2 = new Label("<---- Tableau Piles ---->");
+	    Label Lbl3 = new Label("Freecell Piles ->");
+	    // First name label
+	    GridPane.setHalignment(Lbl1, HPos.RIGHT);
+	    GridPane.setValignment(Lbl1, VPos.CENTER);
+	    gridpane.add(Lbl1, 0, 0);
+	    // Last name label
+	    GridPane.setHalignment(Lbl2, HPos.CENTER);
+	    gridpane.add(Lbl2, 1, 2);
+	 
+	    GridPane.setHalignment(Lbl3, HPos.CENTER);
+	    gridpane.add(Lbl3, 2, 0);
+	    
+		Scene scene = new Scene(stackPane);
+		GridPane.setHalignment(newGame, HPos.CENTER);
+		gridpane.add(newGame, 1, 2);
+		
+		root.setCenter(gridpane);
+		
+		//throwing error here because there's a stage conflict
+		stage.setScene(scene);
+		stage.show();
+	
+		Media sound = new Media("https://www.mfiles.co.uk/mp3-downloads/02.The%20calm%20sea%20floating%20mirage.mp3");
+		mediaPlayer = new MediaPlayer(sound);
+		mediaPlayer.setOnEndOfMedia(new Runnable() {
+			@Override public void run() {
+		         mediaPlayer.seek(Duration.ZERO);
+		       }
+		});
+		mediaPlayer.play(); 
+		
+		Freecell fc = new Freecell(8, 4, 4);
+    	fc.initialSetup();
+	}	
+		
 	
 	
 	private Boolean changeScene(Pane pane){
@@ -206,16 +328,13 @@ public class MainMenu extends Application {
 		Pane layer1 = new Pane();
 		Rectangle rect = new Rectangle(45, 42, Color.BLUE);
 		layer1.getChildren().add(rect);
-
 		Pane layer2 = new Pane();
 		Circle circle = new Circle(30, 20, 10, Color.BLACK);
 		layer2.getChildren().add(circle);
-
 		//TODO: Replace with addAll method once figured out layering. 
 		//Layering occurs like this: index 0 = bottom layer, index 1 = top layer.
 		basePane.getChildren().add(layer1);
 		basePane.getChildren().add(layer2);
-
 		 */	
 
 		//Implicitly calls start()
