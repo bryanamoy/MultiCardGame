0ccedf20dd911b2533058608d41b994790a17ae6
bryanamoy <bryanmoy@gmail.com>
2017-10-26 17:22:41 -0400

    Update SolitaireController.java

--- src/edu/buffalo/cse116/SolitaireController.java
+++ src/edu/buffalo/cse116/SolitaireController.java
@@ -10,47 +10,60 @@ public class SolitaireController {
 	private MainMenu gui;
 	private SolitaireView view;
 	private BakersDozen bd;
+	private Freecell fc;
 	private static final Border UNSELECTED_BORDER = BorderFactory.createEmptyBorder(5, 5, 5, 5);
     private static final Border SELECTED_BORDER = BorderFactory.createMatteBorder(5, 5, 5, 5,
                                                                                 Color.BLUE);
 	
-	public SolitaireController(MainMenu gui, SolitaireView view){
+	public SolitaireController(MainMenu gui, SolitaireView view, BakersDozen bd, Freecell fc){
 		this.gui = gui;
 		this.view = view;
+		this.bd = bd;
+		this.fc = fc;
 		
 		this.view.addSolitaireListener(new SolitaireListener());
 		
 	}
 	class SolitaireListener implements ActionListener{
+		public void selectCard(MainMenu border){
+			border.setBorder(SELECTED_BORDER);
+			border.repaint();
+		}
+		public void unselectCard(MainMenu border){
+			border.setBorder(UNSELECTED_BORDER);
+			border.repaint();
+		}
+		public void getTopTableu(Card card, Card theCard, String whichPile, int whichNumber){
+			bd.getTableauPiles_List();
+			view.setCardImages();
+			fc.addCard(theCard, whichPile, whichNumber);
+			view.setCardImages();
+		}
+		public void addTableau(Card add, int Pile){
+			bd.addCard(add, Pile);
+			view.setCardImages();
+		}
+		public void selectHomecell(){
+			bd.removeFromHomecell(); //returns false, cannot select homecell cards, can't move them
+		}
+		public void addHomecell(Card card, int Pile){
+			if(bd.addToHomecell(card, Pile) == true){
+			bd.addToHomecell(card, Pile);
+			view.setCardImages();
+				//topHomecell = addedcard
+			}
+			else{
+				System.out.println("This move is not legal!");
+			}
+		}
 		
 	}
-	public void selectCard(MainMenu border){
-		border.setBorder(SELECTED_BORDER);
-		border.repaint();
-	}
-	public void unselectCard(MainMenu border){
-		border.setBorder(UNSELECTED_BORDER);
-		border.repaint();
-	}
-	public void getTopTableu(Card card){
-		bd.
-	}
-	public void addTableau(Card card){
-		
-	}
-	public void selectHomecell(){
-		bd.removeFromHomecell();
-	}
-	public void addHomecell(Card card, int Pile){
-		if(bd.addToHomecell(card, Pile) == true){
-		bd.addToHomecell(card, Pile);
-		view.setCardImages();
-			//topHomecell = addedcard
-		}
-		else{
-			System.out.println("This move is not legal!");
-		}
-	}
+	
+	
+	
+	
+	
+	
 	public void updateSolitaireView(){
 		
 	}
