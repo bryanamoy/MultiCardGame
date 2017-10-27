228d0f9afacd3608080e902903beb156b2c3812a
bryanamoy <bryanmoy@gmail.com>
2017-10-27 15:08:04 -0400

    Update SolitaireController2.java

--- src/edu/buffalo/cse116/SolitaireController2.java
+++ src/edu/buffalo/cse116/SolitaireController2.java
@@ -1,15 +1,44 @@
 package edu.buffalo.cse116;
 
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class SolitaireController2{
+
+public class SolitaireController {
 	private MainMenu gui;
 	private SolitaireView view;
 	private BakersDozen bd;
 	private Freecell fc;
+//	private static final Border UNSELECTED_BORDER = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+//    private static final Border SELECTED_BORDER = BorderFactory.createMatteBorder(5, 5, 5, 5,
+//                                                                                Color.BLUE);
+//	
+	public SolitaireController(MainMenu gui, SolitaireView view, BakersDozen bd, Freecell fc){
+		this.gui = gui;
+		this.view = view;
+		this.bd = bd;
+		this.fc = fc;
 		
+		this.view.addSolitaireListener(new SolitaireListener());
+		
+	}		
+	                  
+	
+//		public void selectCard(MainMenu border){
+//			border.setBorder(SELECTED_BORDER);
+//			border.repaint();
+//		}
+//		public void unselectCard(MainMenu border){
+//			border.setBorder(UNSELECTED_BORDER);
+//			border.repaint();
+//		}
 	public void BakersDozenGame(ActionEvent event){
 		BakersDozen bd = new BakersDozen(); //create new BakersDozen game
 		bd.initialSetup();					//set up bakersdozen game
@@ -51,6 +80,17 @@ public class SolitaireController2{
 		}
 	
 	
-	}
 	
+	
+
+	}
+		public void updateSolitaireView(){
+		view.getCardImages();
+	}
+
+
+
+	   
+		
+		}
 }
