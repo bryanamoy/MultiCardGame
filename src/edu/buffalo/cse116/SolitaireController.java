9e43446578ab246e11710e34433cf691522a362e
bryanamoy <bryanmoy@gmail.com>
2017-10-27 18:02:19 -0400

    Update SolitaireController.java

--- src/edu/buffalo/cse116/SolitaireController.java
+++ src/edu/buffalo/cse116/SolitaireController.java
@@ -1,9 +1,18 @@
 package edu.buffalo.cse116;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.swing.BorderFactory;
 import javax.swing.border.Border;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 
 public class SolitaireController {
@@ -11,10 +20,10 @@ public class SolitaireController {
 	private SolitaireView view;
 	private BakersDozen bd;
 	private Freecell fc;
-	private static final Border UNSELECTED_BORDER = BorderFactory.createEmptyBorder(5, 5, 5, 5);
-    private static final Border SELECTED_BORDER = BorderFactory.createMatteBorder(5, 5, 5, 5,
-                                                                                Color.BLUE);
-	
+//	private static final Border UNSELECTED_BORDER = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+//    private static final Border SELECTED_BORDER = BorderFactory.createMatteBorder(5, 5, 5, 5,
+//                                                                                Color.BLUE);
+//	
 	public SolitaireController(MainMenu gui, SolitaireView view, BakersDozen bd, Freecell fc){
 		this.gui = gui;
 		this.view = view;
@@ -23,8 +32,9 @@ public class SolitaireController {
 		
 		this.view.addSolitaireListener(new SolitaireListener());
 		
-	}
-	class SolitaireListener implements ActionListener{
+	}		
+	                  
+	
 		public void selectCard(MainMenu border){
 			border.setBorder(SELECTED_BORDER);
 			border.repaint();
@@ -33,43 +43,66 @@ public class SolitaireController {
 			border.setBorder(UNSELECTED_BORDER);
 			border.repaint();
 		}
-		public void selectTopTableau(Card card, Card theCard, String whichPile, int whichNumber){
-			bd.getTableauPiles_List();
-			
-			fc.addCard(theCard, whichPile, whichNumber);
+	public void BakersDozenGame(ActionEvent event){
+		BakersDozen bd = new BakersDozen(); //create new BakersDozen game
+		bd.initialSetup();					//set up bakersdozen game
+	}
+	public void FreecellGame(ActionEvent event){
+		Freecell fc = new Freecell(8,4,4); //create new freecell game
+		fc.initialSetup();				  //set up freecell game
+	}
+	public void removeHomecell(ActionEvent event){
+	bd.removeFromHomecell(); //returns illegal move
+		fc.removeCard(whichPile, whichNumber);
+	}
 		
+	public void addCard(ActionEvent event){	
+		//try and catch for when a move to homecell pile is illegal
+	try{
+			bd.addToHomecell(card, Pile);	
 		}
-		public void addTableau(Card add, int Pile){
-			try{
-				bd.addCard(add, Pile);
-				
-			}
-			catch(IllegalCardTableauCardException tex){
-				view.displayErrorMessage("Card needs to be one less rank than current top card");
-			}
+		catch(IllegalHomecellCardException hex){
+			view.displayErrorMessage("Card needs to be identical suit and one rank above current top card");
 		}
-		public void selectHomecell(){
-			bd.removeFromHomecell(); //returns false, cannot select homecell cards, can't move them
+		
+	/*	
+	try and catch for when a move to tableau pile is illegal. displayErrorMessage is for
+	for the bullet point asking to display error message for the move	
+	*/
+	try{
+			bd.addCard(add, Pile);
 		}
-		public void addHomecell(Card card, int Pile){
-			try{
-				bd.addToHomecell(card, Pile);
-				
-						//topHomecell = addedcard
-			}
-			catch(IllegalHomecellCardException hex){
-				view.displayErrorMessage("Card needs to be identical suit and one rank above current top card");
-			}
+		catch(IllegalCardTableauCardException tex){
+			view.displayErrorMessage("Card needs to be one less rank than current top card");
 		}
+		
+		try{
+			fc.addCard(theCard, whichPile, whichNumber);
+		}
+		catch(IllegalMoveException ex){
+			view.displayErrorMessage("Error Message");
+		}
+	
+
+	}
+	public void setTableauPile(HashMap<Integer, ArrayList<Card>> tableauPiles_List){
+		bd.setTableauPiles_List(tableauPiles_List);
 		
 	}
+	public void setHomecellPile(HashMap<Integer, ArrayList<Card>> HomcellPiles_List){
+		bd.setHomecellPiles_List(HomcellPiles_List);
+	}
 	
+	//EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>();
+		
 	
-	
-	
-	
-	
-	public void updateSolitaireView(){
+		public void updateSolitaireView(){
 		view.getCardImages();
 	}
+
+
+
+	   
+		
+		
 }
