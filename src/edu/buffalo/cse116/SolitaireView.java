0e434c77c82f460083be9159cbc31f72c9e1f45b
Jonathan Hercules <jherc127@gmail.com>
2017-10-29 17:16:58 -0400

    Finished BD, but freecell not finished

--- src/edu/buffalo/cse116/SolitaireView.java
+++ src/edu/buffalo/cse116/SolitaireView.java
@@ -6,7 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -19,12 +21,15 @@ import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -49,6 +54,8 @@ public class SolitaireView {
 	private BakersDozen bd;
 	private Freecell fc;
 	private MediaPlayer mediaPlayer;
+	private ArrayList<HBox> boxesforBD = new ArrayList<HBox>();
+	private ArrayList<VBox> boxesforFC = new ArrayList<VBox>();
 
 	public SolitaireView() {
 
@@ -124,7 +131,11 @@ public class SolitaireView {
 		return cardImages;
 	}
 
-	public ImageView displayImageViewOfCard(Card c) {
+	public ArrayList<HBox> getBoxesforBD() {
+		return boxesforBD;
+	}
+
+	public ImageView getImageViewOfCard(Card c) {
 		ImageView rtn = null;
 
 		for (Card check : getCardImages().keySet()) {
@@ -142,46 +153,52 @@ public class SolitaireView {
 	}
 
 	public StackPane startBakersDozenGame() {
-
 		StackPane stackPane = new StackPane();
-		
+
 		BorderPane root = new BorderPane();
 		setCardImages("bakersdozen");
-		HashMap<Card, ImageView> Images = getCardImages();
-		ArrayList<VBox> boxes = new ArrayList<VBox>();
 
-		for (Card i : Images.keySet()) {
-			for (ArrayList<Card> pile : bd.getTableauPiles_List().values()) {
-				for (int x = 0; x < 13; x++) {
-					VBox loc = new VBox(10);
-					for (int ind = 0; ind < 4; ind++) {
-						if (pile.get(ind).equals(i)) {
-							loc.getChildren().add(Images.get(i));
-						}
-					}
-					boxes.add(loc);
-				}
+		HashMap<Card, ImageView> Images = getCardImages();
+		ArrayList<Card> cards = new ArrayList<Card>();
+		ArrayList<ImageView> pics = new ArrayList<ImageView>();
+
+		for (ArrayList<Card> pile : bd.getTableauPiles_List().values()) {
+			for (int i = 0; i < 4; i++) {
+				cards.add(pile.get(i));
 			}
 		}
+		for (ImageView image : Images.values()) {
+			image.setFitHeight(50);
+			image.setFitWidth(40);
+		}
 
-		Rectangle r = new Rectangle(900, 500, Color.AQUA);
+		int cursor = 0;
+		for (int x = 0; x < 13; x++) {
+			if (cursor % 4 == 0) {
+				HBox n = new HBox(10);
+				for (int h = 0; h < cards.size(); h++) {
+					for (Card c : Images.keySet()) {
+						if (cards.get(h).toString().equals(c.toString())) {
+							pics.add(Images.get(c));
+						}
+					}
+				}
+				List<ImageView> images = pics.subList(cursor, cursor + 4);
+				for (int ind = 0; ind < 4; ind++) {
+
+					n.getChildren().add(images.get(ind));
+				}
+				boxesforBD.add(n);
+			}
+			cursor += 4;
+		}
+
+		BackgroundFill fill = new BackgroundFill(Color.AQUA, null, null);
+		Background background = new Background(fill);
 		Rectangle test = new Rectangle(25, 40, Color.RED);
-		Rectangle test2 = new Rectangle(25, 40, Color.YELLOW);
+		Rectangle test2 = new Rectangle(25, 40, Color.BLACK);
 		Rectangle test3 = new Rectangle(25, 40, Color.RED);
-		Rectangle test4 = new Rectangle(25, 40, Color.YELLOW);
-		Rectangle test6 = new Rectangle(25, 40, Color.GREEN);
-		Rectangle t2 = new Rectangle(25, 40, Color.RED);
-		Rectangle t3 = new Rectangle(25, 40, Color.YELLOW);
-		Rectangle t4 = new Rectangle(25, 40, Color.RED);
-		Rectangle t5 = new Rectangle(25, 40, Color.YELLOW);
-		Rectangle t6 = new Rectangle(25, 40, Color.RED);
-		Rectangle t7 = new Rectangle(25, 40, Color.YELLOW);
-		Rectangle t8 = new Rectangle(25, 40, Color.RED);
-		Rectangle t9 = new Rectangle(25, 40, Color.YELLOW);
-		Rectangle t10 = new Rectangle(25, 40, Color.RED);
-		Rectangle t11 = new Rectangle(25, 40, Color.YELLOW);
-		Rectangle t12 = new Rectangle(25, 40, Color.RED);
-		Rectangle t13 = new Rectangle(25, 40, Color.YELLOW);
+		Rectangle test4 = new Rectangle(25, 40, Color.BLACK);
 
 		Label title = new Label("Baker's Dozen- to the DEATH");
 		stackPane.setAlignment(title, Pos.TOP_CENTER);
@@ -192,9 +209,6 @@ public class SolitaireView {
 		title.setTextFill(Color.CRIMSON);
 
 		GridPane gridpane = new GridPane();
-
-		ObservableList list = stackPane.getChildren();
-		list.addAll(r, title, gridpane);
 
 		// gridpane grid layout..
 
@@ -218,6 +232,7 @@ public class SolitaireView {
 		ColumnConstraints column8 = new ColumnConstraints();
 		column8.setPercentWidth(12.5);
 		gridpane.getColumnConstraints().addAll(column1, column2, column3, column4, column5, column6, column7, column8);
+
 		RowConstraints row1 = new RowConstraints();
 		row1.setPercentHeight(12.5);
 		RowConstraints row2 = new RowConstraints();
@@ -244,17 +259,10 @@ public class SolitaireView {
 			@Override
 			public void handle(ActionEvent e) {
 				mediaPlayer.stop();
-				startBakersDozenGame();
+				restart("bakersdozen");
 			}
 		});
-		Button quitWD = new Button("Quit with dignity");
-		quitWD.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				// Closes the application
-				Platform.exit();
-			}
-		});
+
 		Button quitNoD = new Button("Just Quit");
 		quitNoD.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -265,28 +273,26 @@ public class SolitaireView {
 		});
 
 		// homecell pile locations
+
 		gridpane.add(test, 1, 0);
 		gridpane.add(test2, 2, 0);
 		gridpane.add(test3, 3, 0);
 		gridpane.add(test4, 4, 0);
 		// tableau pile 1 location
-		gridpane.add(test6, 6, 0);
 
-		
-		 gridpane.add(t2, 3, 2);
-		 gridpane.add(t3, 3, 3);
-		 gridpane.add(t4, 3, 4);
-		 gridpane.add(t5, 3, 5);
-		
-		 gridpane.add(t6, 4, 2);
-		 gridpane.add(t7, 4, 3);
-		 gridpane.add(t8, 4, 4);
-		 gridpane.add(t9, 4, 5);
-		
-		 gridpane.add(t10, 5, 2);
-		 gridpane.add(t11, 5, 3);
-		 gridpane.add(t12, 5, 4);
-		 gridpane.add(t13, 5, 5);
+		gridpane.add(boxesforBD.get(12), 6, 0);
+		gridpane.add(boxesforBD.get(0), 1, 2);
+		gridpane.add(boxesforBD.get(1), 1, 3);
+		gridpane.add(boxesforBD.get(2), 1, 4);
+		gridpane.add(boxesforBD.get(3), 1, 5);
+		gridpane.add(boxesforBD.get(4), 3, 2);
+		gridpane.add(boxesforBD.get(5), 3, 3);
+		gridpane.add(boxesforBD.get(6), 3, 4);
+		gridpane.add(boxesforBD.get(7), 3, 5);
+		gridpane.add(boxesforBD.get(8), 5, 2);
+		gridpane.add(boxesforBD.get(9), 5, 3);
+		gridpane.add(boxesforBD.get(10), 5, 4);
+		gridpane.add(boxesforBD.get(11), 5, 5);
 
 		// Homecell and tableau Labels
 
@@ -296,14 +302,13 @@ public class SolitaireView {
 
 		GridPane.setHalignment(newGame, HPos.CENTER);
 		gridpane.add(newGame, 1, 8);
-		GridPane.setHalignment(quitWD, HPos.LEFT);
-		gridpane.add(quitWD, 4, 8);
+
 		GridPane.setHalignment(quitNoD, HPos.CENTER);
-		gridpane.add(quitNoD, 7, 8);
+		gridpane.add(quitNoD, 4, 8);
 
 		root.setCenter(gridpane);
-		stackPane.getChildren().add(root);
-
+		stackPane.setBackground(background);
+		stackPane.getChildren().addAll(root, title, gridpane);
 		Media sound = new Media("http://www.mfiles.co.uk/mp3-downloads/02.The%20calm%20sea%20floating%20mirage.mp3");
 		playMusic(sound);
 
@@ -315,8 +320,62 @@ public class SolitaireView {
 		StackPane stackPane = new StackPane();
 		setCardImages("freecell");
 		BorderPane root = new BorderPane();
-		Rectangle r = new Rectangle(900, 500, Color.LIGHTGREEN);
 
+		HashMap<Card, ImageView> Images = getCardImages();
+		ArrayList<Card> cards = new ArrayList<Card>();
+		ArrayList<ImageView> pics = new ArrayList<ImageView>();
+
+		for (Integer pile : fc.getTableauMap().keySet()) {
+			if (pile < 5) {
+				for (int x = 0; x < 7; x++) {
+					cards.add(fc.getTableauMap().get(pile).get(x));
+				}
+			} else {
+				for (int ind = 0; ind < 6; ind++) {
+					cards.add(fc.getTableauMap().get(pile).get(ind));
+				}
+			}
+		}
+
+		for (ImageView image : Images.values()) {
+			image.setFitHeight(50);
+			image.setFitWidth(40);
+		}
+		
+		for (int size = 0; size < 52; size++) {
+			for (Card c : Images.keySet()) {
+				if (cards.get(size).toString().equals(c.toString())) {
+					pics.add(Images.get(c));
+				}
+			}
+		}
+		
+		int cursor = 0;
+		for (int x = 0; x < 8; x++) {
+			
+			if (cursor % 7 == 0 && cursor < 28) {
+				VBox box1 = new VBox(5);
+				List<ImageView> images = pics.subList(cursor, cursor + 7);
+				for (int ind = 0; ind < 7; ind++) {
+					box1.getChildren().add(images.get(ind));
+				}
+
+				boxesforFC.add(box1);
+
+				cursor += 7;
+			} else if (cursor % 6 > 0 && cursor < 52) {
+				VBox box1 = new VBox(5);
+				List<ImageView> images = pics.subList(cursor, cursor + 6);
+				for (int ind = 0; ind < 6; ind++) {
+					box1.getChildren().add(images.get(ind));
+				}
+				boxesforFC.add(box1);
+				cursor += 6;
+			}
+
+		}
+
+		Rectangle r = new Rectangle(900, 500, Color.LIGHTGREEN);
 		Rectangle fc1 = new Rectangle(25, 40, Color.RED);
 		Rectangle fc2 = new Rectangle(25, 40, Color.RED);
 		Rectangle fc3 = new Rectangle(25, 40, Color.RED);
@@ -349,9 +408,9 @@ public class SolitaireView {
 
 		// gridpane grid layout..
 
-		gridpane.setPadding(new Insets(25));
-		gridpane.setHgap(5);
-		gridpane.setVgap(25);
+		gridpane.setPadding(new Insets(40));
+		gridpane.setHgap(20);
+		gridpane.setVgap(20);
 		ColumnConstraints column1 = new ColumnConstraints();
 		column1.setPercentWidth(10);
 		ColumnConstraints column2 = new ColumnConstraints();
@@ -429,14 +488,14 @@ public class SolitaireView {
 		gridpane.add(hc3, 8, 0);
 		gridpane.add(hc4, 9, 0);
 
-		gridpane.add(t1, 1, 3);
-		gridpane.add(t2, 2, 3);
-		gridpane.add(t3, 3, 3);
-		gridpane.add(t4, 4, 3);
-		gridpane.add(t5, 5, 3);
-		gridpane.add(t6, 6, 3);
-		gridpane.add(t7, 7, 3);
-		gridpane.add(t8, 8, 3);
+		gridpane.add(boxesforFC.get(0), 1, 3);
+		gridpane.add(boxesforFC.get(1), 2, 3);
+		gridpane.add(boxesforFC.get(2), 3, 3);
+		gridpane.add(boxesforFC.get(3), 4, 3);
+		gridpane.add(boxesforFC.get(4), 5, 3);
+		gridpane.add(boxesforFC.get(5), 6, 3);
+		gridpane.add(boxesforFC.get(6), 7, 3);
+		gridpane.add(boxesforFC.get(7), 8, 3);
 
 		// Homecell and tableau Labels
 
@@ -469,6 +528,15 @@ public class SolitaireView {
 				mediaPlayer.play();
 			}
 		});
+	}
+
+	public void restart(String game) {
+		getCardImages().clear();
+		if (game.equals("bakersdozen")) {
+			startBakersDozenGame();
+		} else if (game.equals("freecell")) {
+			startfreeCellGame();
+		}
 
 	}
 
@@ -476,3 +544,4 @@ public class SolitaireView {
 
 	}
 }
+
