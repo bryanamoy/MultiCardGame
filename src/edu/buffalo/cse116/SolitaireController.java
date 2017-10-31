8c55ab4059f1579ced303fdba177aece2bc647c1
Jonathan Hercules <jherc127@gmail.com>
2017-10-30 23:22:41 -0400

    Update SolitaireController.java

--- src/edu/buffalo/cse116/SolitaireController.java
+++ src/edu/buffalo/cse116/SolitaireController.java
@@ -1,67 +1,35 @@
-package edu.buffalo.cse116;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import javax.swing.BorderFactory;
-import javax.swing.border.Border;
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-
-
-public class SolitaireController {
-	private SolitaireView solitaireView;
-
-	public void selectBorder(StackPane stackPane) {
-		DropShadow dropShadow = new DropShadow(25, Color.YELLOW);
-			//Creating the mouse event handler 
-			EventHandler<MouseEvent> eventHandler = 
-			   new EventHandler<MouseEvent>() {    
-			   @Override 
-			   public void handle(MouseEvent e) { 
-			      stackPane.setEffect(dropShadow);           
-			   } 
-			};    
-			stackPane.addEventHandler(MouseEvent.MOUSE_CLICKED, eventHandler);	
+	private MainMenu gui;
+	private SolitaireView view = new SolitaireView();
+	private BakersDozen bd;
+	private Freecell fc;
+	private DropShadow ds = new DropShadow(20, Color.BLACK);
+	public Node whichClick;
+	
+	public void selected(StackPane s){
+		HashMap<Card,ImageView> images = view.getCardImages();
+		EventHandler<MouseEvent> mouseEntered = new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				s.setEffect(ds);
+			}
+		};
+		EventHandler<MouseEvent> mouseReleased = new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				s.setEffect(null);
+			}
+		};
+		s.setOnMousePressed(mouseEntered);
+		s.setOnMouseReleased(mouseReleased);
 	}
 	
+
+	
 	
 	
 	
 	
-
-	public void highLightBorder(StackPane stackPane) {
-		DropShadow dropShadow = new DropShadow(25, Color.YELLOW);
-			//Creating the mouse event handler 
-		EventHandler<MouseEvent> eventHandler = 
-			   new EventHandler<MouseEvent>() {    
-			   @Override 
-			   public void handle(MouseEvent e) { 
-			      stackPane.setEffect(dropShadow);
-			   } 
-			};  	
-			stackPane.addEventHandler(MouseEvent.MOUSE_CLICKED, eventHandler);	
-		
-	}
-}
-
-	
-
-
-
 
 
 
