210830f566c63aff5afe3fb35e0bf27e1a338da8
Jonathan Hercules <jherc127@gmail.com>
2017-10-30 23:25:22 -0400

    Update SolitaireController.java

--- src/edu/buffalo/cse116/SolitaireController.java
+++ src/edu/buffalo/cse116/SolitaireController.java
@@ -1,4 +1,26 @@
+package edu.buffalo.cse116;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class SolitaireController {
 	private MainMenu gui;
 	private SolitaireView view = new SolitaireView();
 	private BakersDozen bd;
@@ -28,6 +50,9 @@
 	
 	
 	
+	
+	
+}
 	
 	
 
