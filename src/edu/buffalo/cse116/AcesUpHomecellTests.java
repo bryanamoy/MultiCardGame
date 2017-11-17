1a01bf30601adc9918d8e4b64fa7dbf80d74d2d4
captainnicholas <man1@buffalo.edu>
2017-11-17 15:59:31 -0500

    version 2.0

--- src/edu/buffalo/cse116/AcesUpHomecellTests.java
+++ src/edu/buffalo/cse116/AcesUpHomecellTests.java
@@ -13,26 +13,22 @@ public class AcesUpHomecellTests {
 	Card spades = new Card(Suit.SPADES, Rank.EIGHT);
 	Card clubs = new Card(Suit.CLUBS, Rank.SEVEN);
 	ArrayList<Card> testCards = new ArrayList<Card>();
+	BakersDozen test = new BakersDozen();
 	
 	AcesUp auTest = new AcesUp(); //to be changed
 	
 	@Test
 	public void testEmptyHCPile() {
 		auTest.initialSetup();
-		assertEquals("Homecell pile should initiate with zero cards", 0, auTest.getHomecellPiles().get(0));
+		assertEquals("Homecell pile should initiate with zero cards", 0, auTest.getHomecellPiles_List().get(0));
 	}
 	
 	@Test
 	public void testAddSpecificCardLegalOrIllegalHCPile() {
 		auTest.initialSetup();
-		auTest.getHomecellPiles_List();
-		Card first = new Card(Suit.DIAMONDS, Rank.ACE);
-		Card second = new Card(Suit.SPADES, Rank.EIGHT);
-		boolean legal = auTest.addToHomecell(first, 0);
-		boolean illegal = auTest.addToHomecell(second, 0);
-		int sizeofPile = auTest.getHomecellPileSize(0);
-		assertTrue("legal", legal);
-		assertFalse("illegal", illegal);
+		
+		assertTrue("Should always be true", auTest.addto("homecell", 0, 0));
+		
 	}
 	
 	@Test
@@ -40,7 +36,7 @@ public class AcesUpHomecellTests {
 		
 		auTest.initialSetup();
 
-		assertFalse("Should always be false", auTest.removeFromHomecell());
+		assertFalse("Should always be false", auTest.removeFrom("homecell", 0));
 	}
 	
 	@Test
@@ -52,7 +48,7 @@ public class AcesUpHomecellTests {
 		Card second = new Card(Suit.DIAMONDS, Rank.TWO);
 		auTest.addToHomecell(first, 0);
 		auTest.addToHomecell(second, 0);
-		assertEquals("new top card",second, auTest.getHomecellPiles_List().get(0).get(0));
+		assertEquals("new top card", first, auTest.getHomecellPiles_List().get(0).get(0));
 		assertEquals("size of pile", 2, auTest.getHomecellPiles_List().get(0).size());
 
 		
