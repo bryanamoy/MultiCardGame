1da155b17e2bba069a48958a1c6f5013c80e1f07
Jonathan Hercules <jherc127@gmail.com>
2017-11-28 20:44:55 -0500

    Update AcesUpTableauTest.java

--- src/edu/buffalo/cse116/AcesUpTableauTest.java
+++ src/edu/buffalo/cse116/AcesUpTableauTest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AcesUpTableauTest {
+	Card ace = new Card(Suit.CLUBS, Rank.ACE);
+	Card spades = new Card(Suit.SPADES, Rank.EIGHT);
+	Card clubs = new Card(Suit.CLUBS, Rank.SEVEN);
 /*
  * Tests for the single card in each Tableau Pile at the initial start of the game
  */
@@ -12,25 +15,25 @@ public class AcesUpTableauTest {
 	public void intialSetupTablaeuPile1() {
 		AcesUp au = new AcesUp();
 		au.initialSetup();
-		assertEquals(1, au.getSizeOfTableau());
+		assertEquals(1, au.getSizeOfTableau(0));
 	}
 	@Test
 	public void initialSetupTableauPile2(){
 		AcesUp au = new AcesUp();
 		au.initialSetup();
-		assertEquals(1, au.getSizeOfTableau());
+		assertEquals(1, au.getSizeOfTableau(1));
 	}
 	@Test
 	public void initialSetupTableauPile3(){
 		AcesUp au = new AcesUp();
 		au.initialSetup();
-		assertEquals(1, au.getSizeOfTableau());
+		assertEquals(1, au.getSizeOfTableau(2));
 	}
 	@Test
 	public void initialSetupTableauPile4(){
 		AcesUp au = new AcesUp();
 		au.initialSetup();
-		assertEquals(1, au.getSizeOfTableau());
+		assertEquals(1, au.getSizeOfTableau(3));
 	}
 	/*
 	 * Testing that there are 4 Tableau Piles
@@ -39,17 +42,17 @@ public class AcesUpTableauTest {
 	public void initialSetup4TableauPiles(){
 		AcesUp au = new AcesUp();
 		au.initialSetup();
-		assertEquals(4, au.getTableauPiles());
+		assertEquals(4, au.getTableauPiles_List().size());
 	}
 	/*
-	 * Testing if card is legal to add to Tableau Pile
+	 * Testing if card is legal to add to Tableau Pile(Only can add to empty tableau pile)
 	 */
 	@Test
 	public void legalAdd() {
 		AcesUp au = new AcesUp();
 		au.initialSetup();
-		Card card = new Card(Suit.SPADES, Rank.ACE);
-		assertTrue("legal", au.addto(Tableau, 1, au.getTableauPiles()));
+		au.getTableauPiles_List().get(0).clear();
+		assertTrue("legal", au.addto("tableau", 1, 0));
 	}
 	/*
 	 * Testing if card is legal to remove from Tableau Pile
@@ -58,8 +61,11 @@ public class AcesUpTableauTest {
 	public void legalRemove(){
 		AcesUp au = new AcesUp();
 		au.initialSetup();
-		Card card = new Card(Suit.CLUBS, Rank.EIGHT);
-		assertTrue("legal", au.removeFrom(au.getTableauPiles_List(), 2));
+		au.getTableauPiles_List().get(0).clear();
+		au.getTableauPiles_List().get(0).add(ace);
+		au.getTableauPiles_List().get(1).clear();
+		au.getTableauPiles_List().get(1).add(clubs);
+		assertTrue("legal", au.removeFrom("tableau", 0));
 	}
 	/*
 	 * Testing that the size of the Tableau Pile increases when card is added and the new card is
@@ -69,24 +75,29 @@ public class AcesUpTableauTest {
 	public void addIncreaseSize(){
 		AcesUp au = new AcesUp();
 		au.initialSetup();
-		Card card = new Card(Suit.CLUBS, Rank.FOUR);
-		au.addto(Tableau, 1, Tableau);
-		assertEquals(2, au.sizeOfTableau);
+		au.getTableauPiles_List().get(0).clear();
+		au.getTableauPiles_List().get(1).clear();
+		au.getTableauPiles_List().get(1).add(ace);
+		Card checktop = au.getTableauPiles_List().get(1).get(0);
+		au.addto("tableau", 1, 0);
+		assertEquals("Size should be one",1,au.getTableauPiles_List().get(0).size());
+		assertEquals("Should be aces",checktop,au.getTableauPiles_List().get(0).get(0));
 	}
 	/*
 	 * Testing that the size of the Tableau Pile decreases when a card is removed and the next card
 	 * is the Tableau Pile's new top card
 	 */
 	@Test 
+//	Still needs work
 	public void removeDecreaseSize(){
 		AcesUp au = new AcesUp();
 		au.initialSetup();
-		Card card = new Card(Suit.HEARTS, Rank.NINE);
-		Card card2 = new Card(Suit.SPADES, Rank.TEN);
-		au.addto(HEARTSNINE, 2, 2);
-		au.addto(SPADESTEN, 2, 2);
-		au.removeFrom(Tableau, 2);
-		assertEquals(2, au.getSizeOfTableau());
-		assertEquals(HEARTSNINE, au.getTableauPiles_List());
+		au.initialSetup();
+		au.getTableauPiles_List().get(0).clear();
+		au.getTableauPiles_List().get(0).add(ace);
+		au.getTableauPiles_List().get(1).clear();
+		au.getTableauPiles_List().get(1).add(clubs);
+		 au.removeFrom("tableau", 0);
+		assertEquals(0, au.getTableauPiles_List().get(0).size());
 	}
 }
