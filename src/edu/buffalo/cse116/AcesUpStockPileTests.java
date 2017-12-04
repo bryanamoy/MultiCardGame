1ab0bbd1c846de1216919c8adb75da163d52d6c7
bryanamoy <bryanmoy@gmail.com>
2017-12-04 18:06:46 -0500

    Update AcesUpStockPileTests.java

--- src/edu/buffalo/cse116/AcesUpStockPileTests.java
+++ src/edu/buffalo/cse116/AcesUpStockPileTests.java
@@ -16,19 +16,25 @@ public class AcesUpStockPileTests {
 	BakersDozen test = new BakersDozen();
 	
 	AcesUp auTest = new AcesUp(); //to be changed
-	
+	/*
+	 * Testing that the stock pile should hold 48 cards at the beginning of the game
+	 */
 	@Test
 	public void testStockPileInitial() {
 		auTest.initialSetup();
 		assertEquals("Stock pile should initiate with 48 cards", 48, auTest.getStockPile_List().size());
 	}
-	
+	/*
+	 * Testing that adding to a stock pile is an illegal move.  It should always be illegal
+	 */
 	@Test
 	public void testAddingStockPileAlwaysIllegal() {
 		auTest.initialSetup();
 		assertFalse("Should always be false, adding not allowed", auTest.addto("stock", 0, 0));	
 	}
-	
+	/*
+	 * Testing that removing from the stock pile is always a legal move
+	 */
 	@Test
 	public void testRemovingTopCardAlwaysLegalSPile() {
 		
@@ -37,7 +43,10 @@ public class AcesUpStockPileTests {
 		boolean removeTest = auTest.removeFrom("stock", 0);
 		assertTrue("Should be an legal removal, returned" + removeTest, removeTest);
 	}
-	
+	/*
+	 * Testing the function of removing from the stock pile. It should be an illegal move when the 
+	 * stock pile has no more cards.  When cards are removed from the stock pile the size of the tableau pile should increase.
+	 */
 	@Test
 	public void testRemovefromStockPile() {
 		
